0ba6e1b15cc0c99384311647b64c14a7948206e5
bfreder1 <bfreder1@my.westga.edu>
2023-10-29 17:08:07 -0400

    Fixed some Errors and Documentation

--- InfoMgmtFurnitureRentalSystem/DAL/LoginResult.cs
+++ InfoMgmtFurnitureRentalSystem/DAL/LoginResult.cs
@@ -1,20 +1,48 @@
 ﻿using InfoMgmtFurnitureRentalSystem.Model;
 
-namespace InfoMgmtFurnitureRentalSystem.DAL
+namespace InfoMgmtFurnitureRentalSystem.DAL;
+
+/// <summary>
+///     A class that represents the result of a successful login attempt.
+/// </summary>
+public class LoginResult
 {
-    public class LoginResult
+    #region Properties
+
+    /// <summary>
+    ///     Gets the role.
+    /// </summary>
+    /// <value>The role.</value>
+    public Role Role { get; private set; }
+
+    /// <summary>
+    ///     Gets the identifier.
+    /// </summary>
+    /// <value>The identifier.</value>
+    public int Id { get; private set; }
+
+    /// <summary>
+    ///     Gets the username.
+    /// </summary>
+    /// <value>The username.</value>
+    public string Username { get; private set; }
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="LoginResult" /> class.
+    /// </summary>
+    /// <param name="role">The role.</param>
+    /// <param name="id">The identifier.</param>
+    /// <param name="username">The username.</param>
+    public LoginResult(char role, int id, string username)
     {
-        public Role Role { get; private set; }
-
-        public int Id { get; private set; }
-
-        public string Username { get; private set; }
-
-        public LoginResult(char role, int id, string username)
-        {
-            this.Role = RoleExtensions.RoleFromChar(role);
-            this.Id = id;
-            this.Username = username;
-        }
+        this.Role = RoleExtensions.RoleFromChar(role);
+        this.Id = id;
+        this.Username = username;
     }
+
+    #endregion
 }
--- InfoMgmtFurnitureRentalSystem/Model/Furniture.cs
+++ InfoMgmtFurnitureRentalSystem/Model/Furniture.cs
@@ -1,22 +1,57 @@
 ﻿namespace InfoMgmtFurnitureRentalSystem.Model;
 
+/// <summary>
+///     A class that represents a piece of furniture.
+/// </summary>
 public class Furniture
 {
     #region Properties
 
-    public int Id { get; set; }
+    /// <summary>
+    ///     Gets or sets the furniture identifier.
+    /// </summary>
+    /// <value>The furniture identifier.</value>
+    public int FurnitureId { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the category.
+    /// </summary>
+    /// <value>The category.</value>
     public string Category { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the style.
+    /// </summary>
+    /// <value>The style.</value>
     public string Style { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the quantity.
+    /// </summary>
+    /// <value>The quantity.</value>
     public int Quantity { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the rental rate.
+    /// </summary>
+    /// <value>The rental rate.</value>
     public double RentalRate { get; set; }
 
     #endregion
 
     #region Constructors
 
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Furniture" /> class.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    /// <param name="category">The category.</param>
+    /// <param name="style">The style.</param>
+    /// <param name="quantity">The quantity.</param>
+    /// <param name="rentalRate">The rental rate.</param>
     public Furniture(int id, string category, string style, int quantity, double rentalRate)
     {
-        this.Id = id;
+        this.FurnitureId = id;
         this.Category = category;
         this.Style = style;
         this.Quantity = quantity;
